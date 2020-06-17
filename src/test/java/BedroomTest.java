50a6a39f37ad5d4b9736a03ae0dfa8cbf3a058db
mark <markpackham1@gmail.com>
2020-06-17 11:22:11 +0100

    Basic Getter Setter checks not including Guest

--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+    private Bedroom bedroom;
+
+    @Before
+    public void before(){
+        bedroom = new Bedroom(2, "Luxury", "double");
+        //guest = new Guest("Frank");
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(2, bedroom.getCapacity());
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Luxury", bedroom.getName());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("double", bedroom.getType());
+    }
+
+    @Test
+    public void setName(){
+        bedroom.setName("Standard");
+        assertEquals("Standard", bedroom.getName());
+    }
+
+    @Test
+    public void setCapacity(){
+        bedroom.setCapacity(3);
+        assertEquals(3, bedroom.getCapacity());
+    }
+
+    @Test
+    public void setType(){
+        bedroom.setType("single");
+        assertEquals("single", bedroom.getType());
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,50 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+
+    private ConferenceRoom room1;
+    private ConferenceRoom room2;
+
+    @Before
+    public void before() {
+        room1 = new ConferenceRoom(1000, "1a", true);
+        room2 = new ConferenceRoom(1, "2a", false);
+
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(1000, room1.getCapacity());
+    }
+
+    @Test
+    public void canSetCapacity(){
+        room1.setCapacity(2);
+        assertEquals(2, room1.getCapacity());
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("1a", room1.getName());
+    }
+
+    @Test
+    public void canSetName() {
+        room1.setName("100a");
+        assertEquals("100a", room1.getName());
+    }
+
+    @Test
+    public void hasProjector(){
+        assertEquals(true, room1.isHasProjector());
+    }
+
+    @Test
+    public void canSetProjector(){
+        room1.setHasProjector(false);
+        assertEquals(false, room1.isHasProjector());
+    }
 }
